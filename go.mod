41d8486bfa252373e04099e1ff01bb84d6c253ec
taewony <engchat@gmail.com>
2025-02-17 05:00:46 +0900

    module name change

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module go-chitchat
+module github.com/taewony/go-fullstack-webapp
 
 go 1.24.0
+
+require github.com/a-h/templ v0.3.833
--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	templates "github.com/taewony/go-fullstack-webapp/internal/templates" // templates 패키지 import (go.mod 파일 경로에 따라 수정)
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+	templates.Index().Render(r.Context(), w) // index.templ 렌더링
 }
 
 func main() {
-	http.HandleFunc("/", handler) // "/" 경로로 요청이 들어오면 handler 함수 실행
+	http.HandleFunc("/", handler)
 	fmt.Println("chitchat 서버 시작... http://localhost:8080")
-	http.ListenAndServe(":8080", nil) // 8080 포트에서 웹 서버 시작
+	http.ListenAndServe(":8080", nil)
 }
